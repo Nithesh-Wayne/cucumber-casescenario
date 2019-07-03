61c99e6a968791f116b77e97324efddbd77267f4
Nithesh Kg <nitheshkg18@gmail.com>
2019-07-03 17:02:28 +0530

    All features

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features= {"src/test/resources/skeleton/cartadd.feature","src/test/resources/skeleton/searchscenario.feature"},
+@CucumberOptions(features= {"src/test/resources/skeleton/cartadd.feature","src/test/resources/skeleton/searchscenario.feature","src/test/resources/skeleton/Login.feature","src/test/resources/skeleton/registration.feature"},
 				//glue= "skeleton",
 				//plugin="html:target/htmlreport"
 		plugin="json:target/jsonreport.json")
